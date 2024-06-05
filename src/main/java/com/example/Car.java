2a66dc8c2878a0e455650a38c494f494fa741851
yamada-ai <hatsudaaa@gmail.com>
2024-06-05 10:47:27 +0900

    first commit

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Car {
+    private String name;
+    private int speed;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", speed=" + speed +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
---
+++ src/main/java/com/example/GitSampleYamada2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYamada2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYamada2Application.class, args);
+	}
+
+}
